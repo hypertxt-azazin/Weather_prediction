4ba16bd11656835442fb082643d674f8b2695d70
hypertxt-azazin <codingprodg@gmail.com>
2025-03-25 23:31:38 +0000

    defined the neural networks, compiled & trained the model

--- weather_prediction.py
+++ weather_prediction.py
@@ -19,3 +19,17 @@ data[featues] = scaler.fit_transform(data[features])
 x= data[features].values
 y= data[features].values
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.2, random_state=42)
+
+# Define the neural network model
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+    tf.keras.layers.Dense(64, activation='relu'), 
+    tf.keras.layers.Dense(1)
+])
+
+#Compile the model
+model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+
+# Train the model
+history = model.fit(X_train, y_train, epochs=50, batch_size=16, validation_data=(X_test, y_test))
+
